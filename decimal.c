c6634bff95277fd8ae9a5e2f19444a190f138a87
Davidomondi <davifletcher2003@gmail.com>
2023-04-25 07:16:28 -0700

    dec

--- decimal.c
+++ decimal.c
@@ -7,23 +7,9 @@
 
 int print_dec(va_list val)
 {
-	int rem, n, i =0;
+	int n;
 	n = va_arg(val, int);
+	return print_dec(n);
 
-	if  (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		i++;
-
-	}
-	if (n >= 10)
-	{
-		i += print_dec(val);
-	}
-	rem = n % 10;
-	_putchar(rem + '0');
-	i++;
-
-	return (i);
 }
+
