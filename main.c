eae78426b18623dae471c263577d7e8652ba4a6f
David <davifletcher2003@gmail.com>
2023-04-26 15:37:19 +0300

    binary

--- main.c
+++ main.c
@@ -22,6 +22,7 @@ int main(void)
 	 _printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
-    	printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("%b\n", 98);
 	return (0);
 }
